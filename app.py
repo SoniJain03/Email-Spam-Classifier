ccf8515e424aa64cd81f22d9dbf4ff3804235a03
Soni Jain <154066482+SoniJain03@users.noreply.github.com>
2026-07-21 14:27:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pickle
 import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 ps=PorterStemmer()
 
@@ -9,9 +11,6 @@ import nltk
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
-
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 
 def transform_text(text):
   text=text.lower()
